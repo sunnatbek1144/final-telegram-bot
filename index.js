6a049ff952e63a955884a8960d9cd8d7b736b152
Sunnatbek <Sunnatbek912@gmail.com>
2025-12-02 17:36:48 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { config } from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
-config;
+import "./src/bot/bot.js"
+config();
 
 const token = process.env.BOT_TOKEN;
- const bot = new TelegramBot(token, {polling:true})
+const bot = new TelegramBot(token, {polling:true})
+
+
+console.log("Dastur boshlanmoqda ...");
