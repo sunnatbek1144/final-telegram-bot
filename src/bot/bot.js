4f5248bf5a10cb96e0ff1ac9f4645b940843a609
Sunnatbek <Sunnatbek912@gmail.com>
2025-12-18 11:14:04 +0300

    done

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,6 +1,6 @@
 import { config } from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
-// import onProfile from "./handlers/onProfile.js"
+import onStart from "./handlers/onStart.js"
 
 config();
 
@@ -12,14 +12,12 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
   const firstName = msg.chat.first_name;
-bot.sendMessage(chatId, {
-    text: "Hello"
-})
-  // if (text == "/start") {
-  //   return onStart(chatId, firstName)
-  // };
-  // if (text == "/onKurs") {
-  //   return onKurs(chatId, firstName)
-  // }
 
+  
+  if (text == "/start") {
+    return onStart(chatId, firstName)
+  };
+  if (text == "/onKurs") {
+    return onKurs(chatId, firstName)
+  }
 })
--- src/bot/handlers/onStart.js
+++ src/bot/handlers/onStart.js
@@ -1,15 +1,15 @@
-// import { bot } from "../bot.js"
+import { bot } from "../bot.js"
 
-// async function onStart(chatId, firstName) {
-// 	bot.sendMessage(
-// 		chatId,  `👋 Assolomu aleykum ${firstName}!
-// 	📚 Bizning o'qv mazkazga xush kelibsiz!
-// 	Bu bot orqali siz:
-// 	• Kurslarimiz xaqida batafsil ma'lumot olishingiz,
-// 	• Kurslarga onlayn ro'yhatdan otishingiz mumkin, 
-// 	• Dars jadvali va to'lovlar haqida ma'lumot olasiz
+async function onStart(chatId, firstName) {
+	bot.sendMessage(
+		chatId,  `👋 Assolomu aleykum ${firstName}!
+	📚 Bizning o'qv mazkazga xush kelibsiz!
+	Bu bot orqali siz:
+	• Kurslarimiz xaqida batafsil ma'lumot olishingiz,
+	• Kurslarga onlayn ro'yhatdan otishingiz mumkin, 
+	• Dars jadvali va to'lovlar haqida ma'lumot olasiz
 	
-// 	Davom etish uchun pastdan menyuga kiring 😎`,
-// )}
-// export { onStart }
+	Davom etish uchun pastdan menyuga kiring 😎`,
+)}
+export default onStart(msg) 
 
